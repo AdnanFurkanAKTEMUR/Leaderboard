bcd8184b84497a15cafcd896b47bdf57a800c787
AdnanFurkanAKTEMUR <aaktemur@seckin.com.tr>
2023-04-27 09:33:47 +0300

    Update apolloClient.ts

--- client/src/apollo/apolloClient.ts
+++ client/src/apollo/apolloClient.ts
@@ -11,7 +11,7 @@ const defaultOptions: DefaultOptions = {
   },
 };
 const httpLink = new HttpLink({
-  uri: "http://localhost:4000",
+  uri: "http://www.aaktemur.com.tr/graphql",
   credentials: "include"
 });
 
